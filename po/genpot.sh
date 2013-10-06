5f22d65a7ccb0a2724f9da3b228dbce8bc5ad0c5
Muflone <webreg@vbsimple.net>
2013-10-06 13:23:52 +0200

    Removed useless empty translations Added create-translations script Added locale to gitignore

--- po/genpot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-intltool-extract --type=gettext/glade ../data/gextractwinicons.glade
-if [ -f gextractwinicons.pot ]
-then
-  rm gextractwinicons.pot
-fi
-xgettext --language=Python --keyword=_ --keyword=N_ --output gextractwinicons.pot ../data/*.glade.h ../gextractwinicons
-rm ../data/*.glade.h
