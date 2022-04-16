3034f5944a8bb4d13cb22680ba3f83a70e1975f2
Muflone <webreg@muflone.com>
2022-04-16 20:55:09 +0200

    Code cleanup

--- gextractwinicons/__init__.py
+++ gextractwinicons/__init__.py
@@ -17,6 +17,3 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
-
-# Nothing interesting here
-# This is a package placeholder
