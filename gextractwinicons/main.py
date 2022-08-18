a23369e1d9be5158b326ee26f343c4a36a57bc6e
Muflone <webreg@muflone.com>
2022-08-18 19:15:05 +0200

    Moved requirements and translations from package initialization to module main

--- gextractwinicons/main.py
+++ gextractwinicons/main.py
@@ -20,6 +20,7 @@
 
 import logging
 
+import gextractwinicons.requires                                   # noqa: F401
 from gextractwinicons.app import Application
 from gextractwinicons.command_line_options import CommandLineOptions
 from gextractwinicons.constants import (DIR_DATA,
@@ -28,6 +29,7 @@ from gextractwinicons.constants import (DIR_DATA,
                                         DIR_PREFIX,
                                         DIR_SETTINGS,
                                         DIR_UI)
+import gextractwinicons.translations                               # noqa: F401
 
 
 def main():
